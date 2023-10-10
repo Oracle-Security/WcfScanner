00c2a5a60b6a96104f676d12c2c279248ce70f0f
Oracle <122638969+Oracle-Security@users.noreply.github.com>
2023-10-10 10:02:17 -0500

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,33 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
 
-namespace RavenWcfScanner
+namespace FrameworkWcfScanner
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Define list of endpoint names to try
-            List<string> endpointNames = new List<string>
-            {
-                "Service",
-                "MyService",
-                "TestService",
-                "Service.svc",
-                "MyService.svc",
-                "TestService.svc",
-                "Service?wsdl",
-                "MyService?wsdl",
-                "TestService?wsdl"
-            };
+            // Usage: .\FrameworkWcfScanner.exe
+            // If you are compiling from a fresh csproj - Use .net framework 4.8.2 
+            // Endpoint names should have the IP and Port you and trying to scan - ex: 111.111.111.111:808 
+            // Does not currently support Authentication
+            Console.WriteLine("Wcf Scanner");
+            Console.WriteLine("Written by Oracle");
+            Console.WriteLine("For more of my projects visit: www.github.com/Oracle-Security");
 
-            // Define list of IP addresses to scan
-            List<string> ipAddresses = new List<string>
-            {
-                "10.80.34.2"
-            };
+            string[] endpointNames = System.IO.File.ReadAllLines("wcf_endpointnames.txt");
+            string[] ipAddresses = System.IO.File.ReadAllLines("ip_addresses.txt");
+
+            // List of extensions
+            string[] extensions = { ".svc", "?wsdl", "" };
 
             // Define dictionary of endpoint URL schemes for each binding type
             Dictionary<Type, string> endpointSchemes = new Dictionary<Type, string>
@@ -35,40 +32,29 @@ namespace RavenWcfScanner
                 { typeof(BasicHttpBinding), "http" },
                 { typeof(NetTcpBinding), "net.tcp" },
                 { typeof(WSHttpBinding), "http" },
-                { typeof(NetNamedPipeBinding), "net.pipe" }
-                //{ typeof(NetMsmqBinding), "net.msmq" }
+                { typeof(NetNamedPipeBinding), "net.pipe" },
+                { typeof(NetMsmqBinding), "net.msmq" }
             };
 
-            // Define credentials for authentication
-            // add this in Cli 
-            const string username = "test";
-            const string password = "test";
-
-            foreach (string ipAddress in ipAddresses)
+        foreach (string ipAddress in ipAddresses)
+        {
+            foreach (string endpointName in endpointNames)
             {
-                foreach (string endpointName in endpointNames)
+                foreach (string extension in extensions) 
                 {
                     foreach (Type bindingType in endpointSchemes.Keys)
                     {
                         // Construct the endpoint URL using the IP address, endpoint name, and binding type
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
                         Binding binding = (Binding)Activator.CreateInstance(bindingType);
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
                         string endpointScheme = endpointSchemes[binding.GetType()];
-                        string endpointUrl = $"{endpointScheme}://{ipAddress}:808/{endpointName}";
+                        string endpointUrl = $"{endpointScheme}://{ipAddress}/{endpointName}{extension}";
 
-                        // Create creds for auth
-                        var credentials = new ClientCredentials();
-                        credentials.UserName.UserName = username;
-                        credentials.UserName.Password = password;
-
-                        // Attempt to connect to the endpoint URL using the binding and credentials
+                        // Attempt to connect to the endpoint URL using the binding
                         try
                         {
                             using (var factory = new ChannelFactory<IMyService>(binding, new EndpointAddress(endpointUrl)))
                             {
-                                factory.Endpoint.EndpointBehaviors.Add(new ClientCredentialsEndpointBehavior(credentials));
                                 var client = factory.CreateChannel();
                                 try
                                 {
@@ -76,7 +62,12 @@ namespace RavenWcfScanner
                                     string result = client.GetData(42);
 
                                     // If the call is successful, log the result
-                                    Console.WriteLine($"Valid endpoint URL: {endpointUrl}");
+                                    Console.WriteLine($"Valid endpoint URL: {endpointUrl} with binding {binding.GetType().Name}");
+                                }
+                                catch (Exception ex)
+                                {
+                                    // Log any exceptions that occur while calling the service
+                                    Console.WriteLine($"Error calling service at endpoint URL {endpointUrl} with binding {binding.GetType().Name}: {ex.Message}");
                                 }
                                 finally
                                 {
@@ -95,48 +86,12 @@ namespace RavenWcfScanner
             }
         }
     }
+}
 
-    [ServiceContract]
+[ServiceContract]
     public interface IMyService
     {
         [OperationContract]
         string GetData(int value);
-    }
-
-    public class ClientCredentialsEndpointBehavior : IEndpointBehavior
-    {
-        private readonly ClientCredentials _credentials;
-
-        public ClientCredentialsEndpointBehavior(ClientCredentials credentials)
-        {
-            _credentials = credentials;
-        }
-
-        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
-        {
-        }
-
-        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientCredentials clientCredentials)
-        {
-            clientCredentials.UserName.UserName = _credentials.UserName.UserName;
-            clientCredentials.UserName.Password = _credentials.UserName.Password;
-        }
-
-        void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
-        {
-            ApplyDispatchBehavior(endpoint, endpointDispatcher);
-        }
-        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
-        {
-        }
-
-        public void Validate(ServiceEndpoint endpoint)
-        {
-        }
-
-        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
